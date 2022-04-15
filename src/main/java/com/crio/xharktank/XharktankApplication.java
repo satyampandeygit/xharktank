69d47b1908dfe759466bcb9ddc5aedf3136662a5
psatyam360 <psatyam360@gmail.com>
2022-04-15 19:26:44 +0530

    resolving cors issue

--- src/main/java/com/crio/xharktank/XharktankApplication.java
+++ src/main/java/com/crio/xharktank/XharktankApplication.java
@@ -18,7 +18,7 @@ public class XharktankApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*");
+				registry.addMapping("https://xharktank.crio.do/").allowedOrigins("*");
 			}
 
 		};
