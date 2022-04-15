a3d99fc7bb3afe10d7530d5799b0a29be5768b8b
psatyam360 <psatyam360@gmail.com>
2022-04-15 19:15:54 +0530

    resolving cors issue

--- src/main/java/com/crio/xharktank/XharktankApplication.java
+++ src/main/java/com/crio/xharktank/XharktankApplication.java
@@ -2,11 +2,9 @@ package com.crio.xharktank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class XharktankApplication {
@@ -16,16 +14,14 @@ public class XharktankApplication {
 	}
 
 	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		source.registerCorsConfiguration("/**", config);
-		registrationBean.setFilter(new CorsFilter(source));
-		registrationBean.setOrder(0);
-		return registrationBean;
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+
+		};
 	}
 
 }
