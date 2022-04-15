f0ef3f75afc68dde6ec7e19b0986d8e54efdef5e
psatyam360 <psatyam360@gmail.com>
2022-04-15 20:15:46 +0530

    resolving cors issue

--- src/main/java/com/crio/xharktank/XharktankApplication.java
+++ src/main/java/com/crio/xharktank/XharktankApplication.java
@@ -2,26 +2,27 @@ package com.crio.xharktank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-@CrossOrigin(origins="*")
 public class XharktankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XharktankApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer configure() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/*").allowedOrigins("https://xharktank.crio.do/");
-//			}
-//
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("https://xharktank.crio.do");
+			}
+
+		};
+	}
 
 }
